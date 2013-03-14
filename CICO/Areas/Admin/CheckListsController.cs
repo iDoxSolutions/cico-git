2c4ef9f66cb9d587a39f5a320e59c3595ea2fdc9
Pawel Wasilewski <wasilewski.pawel@gmail.com>
2013-03-14 15:33:01 +0100

    file submition

---
+++ CICO/Areas/Admin/CheckListsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Cico.Areas.Admin
+{
+    public class CheckListModel
+    {
+        public string EmployeeName { get; set; }
+    }
+    public class CheckListsModel
+    {
+        public IList<CheckListModel> CheckListModels { get; set; }
+    }
+
+    public class CheckListsController : Cico.Controllers.ControllerBase
+    {
+        //
+        // GET: /CheckLists/
+
+        public ActionResult Index()
+        {
+            var model = new CheckListsModel()
+                {
+                    
+                };
+            var sessions = Db.CheckListSessions.Include("Employee").ToList();
+            model.CheckListModels = sessions.Select(c => new CheckListModel
+                {
+                    EmployeeName = c.Employee.Surname + ", " + c.Employee.GivenName
+                }).ToList();
+            return View(model);
+        }
+
+    }
+}
