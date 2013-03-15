44c356fd8b70ee2a103c645b66601e3d8c204cc0
Pawel Wasilewski <wasilewski.pawel@gmail.com>
2013-03-15 09:50:53 +0100

    modifications

--- CICO/Scripts/Forms/DocumentSubmittedModel.js
+++ CICO/Scripts/Forms/DocumentSubmittedModel.js
@@ -21,13 +21,6 @@
             enctype: 'multipart/form-data',
             // other available options: 
             url: "/checklist/UploadFile",   //  ,    // override for form's 'action' attribute 
-            //type:      type        // 'get' or 'post', override for form's 'method' attribute 
-            //dataType:  null        // 'xml', 'script', or 'json' (expected server response type) 
-            //clearForm: true        // clear all form fields after successful submit 
-            //resetForm: true        // reset the form after successful submit 
-
-            // $.ajax options can be used here too, for example: 
-            //timeout:   3000 ,
             data: { itemTemplateId: item.item.Id }
         };
         // $(e).ajaxForm(options);
