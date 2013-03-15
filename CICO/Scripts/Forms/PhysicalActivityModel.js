b646cd42ac7091b19c26df13ca2f6df89364b890
Pawel Wasilewski <wasilewski.pawel@gmail.com>
2013-03-15 09:46:00 +0100

    problem solved

--- CICO/Scripts/Forms/PhysicalActivityModel.js
+++ CICO/Scripts/Forms/PhysicalActivityModel.js
@@ -2,7 +2,8 @@
     var self = this;
     self.FormBase = new CicoFormBase(item);
     self.templateName = "PhysicalActivity";
-    self.submitDoc = function () {
-
+    self.complete = function () {
+        item.Checked(true);
+        alert('');
     };
 }
