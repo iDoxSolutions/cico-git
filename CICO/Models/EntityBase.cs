24475f421c77bc138a90a39ffb4a047a57e52938
nvcook <nataliacook@aol.com>
2014-01-13 12:39:29 -0600

    Update EntityBase.cs

--- CICO/Models/EntityBase.cs
+++ CICO/Models/EntityBase.cs
@@ -8,13 +8,18 @@ namespace Cico.Models
 {
     public class EntityBase
     {
-        public EntityBase()
+          public EntityBase()
         {
             Active = true;
         }
+        [Display(Name = "CheckList Date Created: Format mm/dd/yyyy")]
+        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for Date Created.")]
         public DateTime? DateCreated { get; set; }
         [StringLength(100)]
         public string UserCreated { get; set; }
+
+        [Display(Name = "CheckList Date Edited: Format mm/dd/yyyy")]
+        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for Date Edited.")]
         public DateTime? DateEdited { get; set; }
         [StringLength(100)]
         public string UserEdited { get; set; }
