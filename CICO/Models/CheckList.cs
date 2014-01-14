46c96630ed1f55518b61b01b2a62c11a9c99f8ea
nvcook <nataliacook@aol.com>
2014-01-14 15:01:46 -0600

    Update CheckList.cs

--- CICO/Models/CheckList.cs
+++ CICO/Models/CheckList.cs
@@ -5,7 +5,7 @@ namespace Cico.Models
 {
     public class CheckList
     {
-       [Key]
+        [Key]
         public int CheckListId { get; set; }
         [Required]
         [Display(Name = "CheckList Type")]
@@ -14,13 +14,13 @@ namespace Cico.Models
         [Display(Name = "CheckList Description")]
         public string Name {get; set; }
         [Display(Name = "CheckList Start Date: Format mm/dd/yyyy")]
-        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for Start Date.")]
+        [RegularExpression("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$", ErrorMessage = "Invalid format for Start Date.")]
         public DateTime? StartDate { get; set; }
         [Display(Name = "CheckList EndDate: Format mm/dd/yyyy")]
-        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for End Date.")]
+        [RegularExpression("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$", ErrorMessage = "Invalid format for End Date.")]
         public DateTime? EndDate { get; set; }
         [Display(Name = "CheckList Due Date: mm/dd/yyyy")]
-        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for Due Date.")]
+        [RegularExpression("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$", ErrorMessage = "Invalid format for Due Date.")]
         public DateTime? DueDate { get; set; }
         [Display(Name = "CheckList Status")]
         public String Status { get; set; }
