dc7ddf74ef91e8f6d0b762ba88d7614c79e4e14f
nvcook <nataliacook@aol.com>
2014-01-14 15:25:40 -0600

    Update InitModel.cs

--- CICO/Models/Authentication/InitModel.cs
+++ CICO/Models/Authentication/InitModel.cs
@@ -6,10 +6,10 @@ namespace Cico.Models.Authentication
 {
     public class InitModel
     {
-        [DisplayName("Arrival Date" + "*Enter best estimate. Can be changed later")]
+         [DisplayName("Arrival Date" + "*Enter best estimate. Can be changed later")]
         [Required]
         public DateTime ArrivalDate { get; set; }
-        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for Arrival Date.")]
+        [RegularExpression("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$", ErrorMessage = "Invalid format for Arrival Date.")]
         [StringLength(65)]
         [Required]
         [DisplayName("First Name")]
