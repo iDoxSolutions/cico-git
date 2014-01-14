f87ff188f391cfbfd1b6437720e91af036250117
nvcook <nataliacook@aol.com>
2014-01-14 15:22:17 -0600

    Update EntityBase.cs

--- CICO/Models/EntityBase.cs
+++ CICO/Models/EntityBase.cs
@@ -8,18 +8,18 @@ namespace Cico.Models
 {
     public class EntityBase
     {
-          public EntityBase()
+           public EntityBase()
         {
             Active = true;
         }
         [Display(Name = "CheckList Date Created: Format mm/dd/yyyy")]
-        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for Date Created.")]
+        [RegularExpression("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$", ErrorMessage = "Invalid format for Date Created.")]
         public DateTime? DateCreated { get; set; }
         [StringLength(100)]
         public string UserCreated { get; set; }
 
         [Display(Name = "CheckList Date Edited: Format mm/dd/yyyy")]
-        [RegularExpression("^([0-9]{1,2})[./-]+([0-9]{1,2})[./-]+([0-9]{2}|[0-9]{4})$", ErrorMessage = "Invalid format for Date Edited.")]
+        [RegularExpression("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$", ErrorMessage = "Invalid format for Date Edited.")]
         public DateTime? DateEdited { get; set; }
         [StringLength(100)]
         public string UserEdited { get; set; }
