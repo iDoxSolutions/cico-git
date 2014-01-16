99b63be3053c8081dc5c572dd3ad0a9a069480f4
nvcook <nataliacook@aol.com>
2014-01-16 15:15:44 -0600

    Update AccountModels.cs

--- CICO/Models/AccountModels.cs
+++ CICO/Models/AccountModels.cs
@@ -51,6 +51,7 @@ namespace Cico.Models
         [Required]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
+        [RegularExpression("^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$", ErrorMessage = "Invalid e-mail.")]
         public string Email { get; set; }
 
         [Required]
